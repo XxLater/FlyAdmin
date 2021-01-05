d6180b01d753d49efcaecdc73fca7ccc74c5db43
Echo <945462788@qq.com>
2021-01-05 18:29:01 +0800

    添加token中间件

---
+++ app/common/middleware/TokenMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @class
+ * @author echo
+ * @email 945462788@qq.com
+ * @github https://github.com/945462788
+ **/
+
+
+namespace app\common\middleware;
+
+
+use app\common\service\system\TokenService;
+use app\common\service\user\UserService;
+use app\Request;
+use think\Response;
+
+class TokenMiddleware extends BaseMiddleware
+{
+
+    public function before(Request $request):void
+    {
+        $token = $this->request->header('Token');
+
+        if (!$token) app('response')->fail('缺少token参数',[],40002);
+
+        if (!$uid = TokenService::instance()->verify($token)) app('response')->fail('token验证失败',[],40003);
+
+        $user = UserService::instance()->userIdByUser($uid);
+
+        if ($user->isEmpty()) app('response')->fail('用户不存在',[],40003);
+
+        $request->user_id = $uid;
+    }
+
+    public function after(Response $response):void
+    {
+
+    }
+}
--- route/api.php
+++ route/api.php
@@ -9,10 +9,31 @@ declare(strict_types=1);
  * @github https://github.com/945462788
  **/
 use think\facade\Route;
+use app\common\middleware\TokenMiddleware;
 
-//Route::group('api/',function (){
-//
-//})->middleware();
+Route::group('api/',function (){
 
-Route::any('wechat/app-login','api.Auth/weChatAppLogin');
+    //需要登录
+    Route::group(function (){
+
+        //用户
+        Route::group('user/',function (){
+            Route::get('info','getUserInfo');
+        })->prefix('api.user.User/');
+
+    })->middleware(TokenMiddleware::class);
+
+    //不需要登录
+    Route::group(function (){
+        Route::post('wechat/app_login','api.Auth/weChatAppLogin');
+        Route::post('wechat/login','api.Auth/weChatLogin');
+        Route::group('resources/',function (){
+            Route::get('wechat_js_sdk','WeChatJsSdkConfig');
+            Route::get('token','getToken');
+        })->prefix('api.Resources/');
+    });
+
+})->middleware(\app\common\middleware\AllowOriginMiddleware::class);
+
+
 
